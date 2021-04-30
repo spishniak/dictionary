b1e2e1ccc18b18c847d6077435db98d57b11e337
ivan <spishniak@gmail.com>
2021-04-30 09:50:09 +0300

    posibility to call countByLetter() without args

--- src/index.js
+++ src/index.js
@@ -2,18 +2,25 @@ const fs = require('fs')
 
 
 
-function countByLetter(letter){
+function countByLetter(letter) {
     const dictionariStr = fs.readFileSync('../dictionary.txt').toString()
     const allWords = dictionariStr.split('\n')
-    let withLetterCounter = 0
-    let totalCounter = 0
-    allWords.forEach(word => {
-        totalCounter += 1 
-        if(word[0] === letter){
-            withLetterCounter += 1
+    let counter = 0
+    let isAnyMatches = false
+
+    for (let i = 0; i < allWords.length; ++i) {
+        const word = allWords[i]
+
+        if (letter === undefined) {
+            counter += 1
+        } else if (word[0] === letter) {
+            counter += 1
+            isAnyMatches = true
+        } else if (isAnyMatches) {
+            break
         }
-    })
-    console.log(`Total words: ${totalCounter}\nStarted from '${letter}': ${withLetterCounter}`)
+    }
+    console.log(`result: ${counter}`)
 
 }
 countByLetter('a')
