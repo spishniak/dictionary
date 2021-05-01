21e66cf1a63f9eab814e72e88470475fb8b4c8be
ivan <spishniak@gmail.com>
2021-05-01 13:33:24 +0300

    create service for all functions

--- src/index.js
+++ src/index.js
@@ -1,39 +1,4 @@
-const fs = require('fs')
+const service = require('./service')
 
 
-
-function countAllWordStartingFrom(letter) {
-    const dictionariStr = fs.readFileSync('../dictionary.txt').toString()
-    const allWords = dictionariStr.split('\n')
-    let counter = 0
-    let isAnyMatches = false
-
-    for (let i = 0; i < allWords.length; ++i) {
-        const word = allWords[i]
-
-        if (letter === undefined) {
-            counter += 1
-        } else if (word[0] === letter) {
-            counter += 1
-            isAnyMatches = true
-        } else if (isAnyMatches) {
-            break
-        }
-    }
-    console.log(`result: ${counter}`)
-
-}
-function isWordExist(word) {
-    const dictionariStr = fs.readFileSync('../dictionary.txt').toString()
-    const allWords = dictionariStr.split('\n')
-    const result = allWords.some((dictWord) => dictWord === word)
-    console.log(result ? 'Yes' : 'No')
-}
-
-function isWords(word) {
-    const dictionariStr = fs.readFileSync('../dictionary.txt').toString()
-    const allWords = dictionariStr.split('\n')
-    const result = allWords.filter((collocation) => collocation.includes(word))
-    console.log(result.length)
-}
-isWords('a') 
+service.countAllWordStartingFrom('z')
---
+++ src/service.js
@@ -0,0 +1,44 @@
+const fs = require('fs')
+
+
+
+function countAllWordStartingFrom(letter) {
+    const dictionariStr = fs.readFileSync('../dictionary.txt').toString()
+    const allWords = dictionariStr.split('\n')
+    let counter = 0
+    let isAnyMatches = false
+
+    for (let i = 0; i < allWords.length; ++i) {
+        const word = allWords[i]
+
+        if (letter === undefined) {
+            counter += 1
+        } else if (word[0] === letter) {
+            counter += 1
+            isAnyMatches = true
+        } else if (isAnyMatches) {
+            break
+        }
+    }
+    console.log(`result: ${counter}`)
+
+}
+function isWordExist(word) {
+    const dictionariStr = fs.readFileSync('../dictionary.txt').toString()
+    const allWords = dictionariStr.split('\n')
+    const result = allWords.some((dictWord) => dictWord === word)
+    console.log(result ? 'Yes' : 'No')
+}
+
+function countWordsWithSubstring(substring) {
+    const dictionariStr = fs.readFileSync('../dictionary.txt').toString()
+    const allWords = dictionariStr.split('\n')
+    const result = allWords.filter((dictWord) => dictWord.includes(substring))
+    console.log(result.length)
+}
+
+module.exports = {
+    countAllWordStartingFrom,
+    isWordExist,
+    countWordsWithSubstring,
+}
